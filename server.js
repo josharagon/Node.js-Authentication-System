1d7237ad6aadcb70dbc4a11fefb168d88538cac0
Josh Aragon <josharagon99@gmail.com>
2021-03-15 17:41:19 -0600

    Add error message if email or password are incorrect on login

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(methodOverride('_method'))
 
 
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', {name: req.user.name })
+    res.render('index.ejs', { name: req.user.name })
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
